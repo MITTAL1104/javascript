e58592cdde5be82424ee87ed0a52805600622b89
MITTAL1104 <99600444+MITTAL1104@users.noreply.github.com>
2024-10-21 08:17:46 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountID = 144553
+
+let accountEmail = "raghavklk11@gmail.com"
+
+var accountPassword = "12345"
+
+accountCity = "Patiala"
+
+//accountID = 2
+
+accountEmail = "rmittal1_be21@thapar.edu"
+accountPassword="2121"
+accountCity = "Kalka"
+
+let accountState;
+//console.log(accountID);
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
+
+
+/*
+Prefer not to use var due to block scope and functional scope issue
+*/
