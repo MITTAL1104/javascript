d03cd3d5c20398311f9089c7cc7b0db31e143318
MITTAL1104 <99600444+MITTAL1104@users.noreply.github.com>
2024-10-21 08:18:08 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+ "use strict"; //treat all JS code as newer version
+
+ //alert("hello")
+
+ console.log(3+3);
+
+ console.log("Hitesh");
+
+ let age = 18
+ let isLoggedIn = false
+ let state = null
+ 
+ //number => 2 to power 53
+ //bigint
+ //string => ""
+ //boolean => true/false
+ //null=> standalone value
+ //undefined =>
+ //symbol => unique
+
+ //object
+
+ console.log(typeof undefined);
+ 
