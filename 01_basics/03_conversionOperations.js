42a27fad9031bf529dd7d96c2677c38f088f1fdb
MITTAL1104 <99600444+MITTAL1104@users.noreply.github.com>
2024-10-21 10:58:14 +0000

    datatypes_completed

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,48 @@
+/*
+let score = true
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueInNumber=Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;
+// false => 0;
+
+let isLoggedIn = 8
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+//ANYTHING OTHER THAN EMPTY STRING AND 0 
+//RETURNS TRUE 
+
+
+//**************OPERATIONS*************
+
+let value = 3
+let negValue= -value;
+//console.log(negValue);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log((3+4)*5%3); //2
+
+// console.log(+true);
+// console.log(+"");
+
+
+
+
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+console.log(null>0);
+
+console.log(null==0);
+
+console.log(null>=0);
+
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
+//strict check ,checks value as well as data type
+console.log("2"===2);
