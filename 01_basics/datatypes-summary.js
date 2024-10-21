53e1ac23b8fcbc6e12ba961f5ce5b9fafd9d0fa6
MITTAL1104 <raghavklk11@gmail.com>
2024-10-22 00:06:08 +0530

    today last

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -32,6 +32,9 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
+console.log(myObj.name);
+
+
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
