bb24273f87c4969fed62d668980ae6a426d57256
MITTAL1104 <raghavklk11@gmail.com>
2024-10-22 22:09:57 +0530

    22 oct final

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,3 +1,5 @@
+/*
+
 const score = 400;
 console.log(score);
 
@@ -6,6 +8,35 @@ console.log(balance);
 console.log(balance.toString().length);
 console.log(balance.toFixed(1));
 
-const otherNumber  = 123.8966
+const otherNumber  = 1123.8966
 
 console.log(otherNumber.toPrecision(3));
+
+const hundereds = 1000000
+console.log(hundereds.toLocaleString('en-US'));
+*/
+
+//**************MATHS*************//
+
+//console.log(Math);
+
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+//random generates a vlaue between 0 and 1 but to ensure 
+//that the value is atleast 1 we did this
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
---
+++ 01_basics/07_dates.js
@@ -0,0 +1,9 @@
+//Everything defined with beginning reference to be January 1,1970
+
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
