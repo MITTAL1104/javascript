7ff1f4ce19a79713da61287187e5718e5cbbc3af
MITTAL1104 <raghavklk11@gmail.com>
2024-10-25 15:13:10 +0530

    25oct

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -2,8 +2,32 @@
 
 let myDate = new Date()
 
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23);
+// //let myCreatedDate1 = new Date(2023,0,23,5,3);
+// let myCreatedDate2 = new Date("2023-01-14");
+// console.log(myCreatedDate.toDateString());
+// //console.log(myCreatedDate1.toLocaleString());
+// console.log(myCreatedDate2.toLocaleString());
+
+
+let myCreatedDate = new Date("01-14-2023")
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+console.log(Date.now()/1000);
+
+const newDate = new Date();
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+})
+
 
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,10 @@
+// const myArr = [0,1,2,true,4]
+
+// // here arrays can contain different data types
+
+// console.log(myArr[3]);
+
+// console.log(myArr.concat());
+
+
+
