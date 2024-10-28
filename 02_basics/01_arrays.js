fc344d1b0c506a6a325fd5251dc58a6c6c1b9b27
MITTAL1104 <raghavklk11@gmail.com>
2024-10-28 12:30:11 +0530

    28oct commit

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,10 +1,29 @@
-// const myArr = [0,1,2,true,4]
+const myArr = [0,1,2,true,4,"Raghav"];
 
-// // here arrays can contain different data types
+// here arrays can contain different data types
 
-// console.log(myArr[3]);
+//console.log(myArr[3]);
 
-// console.log(myArr.concat());
+
+//Array Methods
+
+myArr.push(6);  //to insert at array end
+myArr.pop()
+
+myArr.unshift(9);  // to insert at array beginning
+myArr.shift(); // to remove from array begining  
+
+// console.log(myArr.includes(6));
+// console.log(myArr.indexOf(true));
+console.log("A",myArr);
+
+const mnew1 = myArr.slice(1,2);
+console.log(mnew1);
+// const newArr = myArr.join()
+// console.log( typeof newArr);
+//console.log(myArr.concat());      
+
+
 
 
 
