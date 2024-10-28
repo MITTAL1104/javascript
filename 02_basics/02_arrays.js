8d494b7e3d7d554c651518297f471320f621635d
MITTAL1104 <99600444+MITTAL1104@users.noreply.github.com>
2024-10-28 15:00:50 +0000

    arrays and objects

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,31 @@
+const marvel_heros = ["Thor","Ironman","Spiderman"]
+
+const dc_heros= ["Superman","Flash","Batman"]
+
+//marvel_heros.push(dc_heros); 
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][2]);
+
+// const ans = marvel_heros.concat(dc_heros)
+// console.log(ans);
+
+// const all_spread = [...marvel_heros,...dc_heros]
+// console.log(all_spread);
+
+// const another_Array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_Array.flat(Infinity)
+// console.log(real_another_array);
+
+//console.log(Array.isArray("Raghav"));
+console.log(Array.from({name:"Raghav"})); // need to tell whether to make an array of keys or values 
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
+
+
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,73 @@
+//const tinderuser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Raghav",
+            lastname:"Mittal"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+
+//const obj3 = {obj1,obj2};
+//const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = {...obj1,...obj2}
+//console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"a@gmail.com"
+    },
+    {
+        id:2,
+        email:"b@gmail.com"
+    },
+    {
+        id:3,
+        email:"c@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log((tinderUser.hasOwnProperty('isLogged')));
+
+
+const course = {
+    courseName: "Javascript",
+    price:"999",
+    courseInstructor: "Raghav"
+}
+
+//course.courseInstructor
+
+const {courseInstructor:Instr} = course
+
+console.log(Instr);
+
+{
+    "name":"Raghav",
+    "coursename":"Javascript",
+    "price":"free"
+}
